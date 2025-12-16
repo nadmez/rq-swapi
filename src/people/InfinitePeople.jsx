9701b43f9c50c58d8db8cd7242966582099379f3
nadmez <nadmez@yahoo.com>
2025-12-16 16:16:23 +0300

    Infinite scroll for people is implemented

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.py4e.com/api/people/";
@@ -9,5 +10,31 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+
+  const { data, fetchNextPage, hasNextPage, isFetching } = useInfiniteQuery({
+    queryKey: ["sw-people"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => lastPage.next || undefined,
+  });
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+      >
+        {data?.pages?.map((page, pageIndex) => (
+          <div key={pageIndex}>
+            {page.results.map((person) => (
+              <Person key={person.name + "_" + pageIndex} {...person} />
+            ))}
+          </div>
+        ))}
+      </InfiniteScroll>
+    </>
+  );
 }
