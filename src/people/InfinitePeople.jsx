5366803086781403a34bf377330a6a3d5012688f
nadmez <nadmez@yahoo.com>
2025-12-16 15:18:13 +0300

    api url update

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,7 +1,7 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = "https://swapi.py4e.com/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
