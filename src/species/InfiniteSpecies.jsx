edb9c08e5a55b83091c2a98d048d03216f4608d9
nadmez <nadmez@yahoo.com>
2025-12-19 16:23:10 +0300

    Proje sonu

--- src/species/InfiniteSpecies.jsx
+++ src/species/InfiniteSpecies.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.py4e.com/api/species/";
 const fetchUrl = async (url) => {
@@ -8,6 +9,49 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isError,
+    error,
+    isLoading,
+  } = useInfiniteQuery({
+    queryKey: ["infinite-species"],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div className="error">{error.message}</div>;
+  }
+
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+
+      <InfiniteScroll
+        initialLoad={false}
+        loadMore={() => {
+          if (!isFetching) fetchNextPage();
+        }}
+        hasMore={hasNextPage}
+      >
+        {data?.pages?.map((page, pageIndex) => (
+          <div key={pageIndex}>
+            {page.results.map((species) => (
+              <Species key={species.name + "_" + pageIndex} {...species} />
+            ))}
+          </div>
+        ))}
+      </InfiniteScroll>
+    </>
+  );
 }
